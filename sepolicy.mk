2098133ed3e1337d9a683bd1d043043144463717
Andreas Schneider <asn@cryptomilk.org>
2020-03-12 18:45:17 +0100

    The big bang!
    Change-Id: I7a030e4dcc1115fc7ccf067272e4adb73ebe3af8

---
+++ sepolicy.mk
@@ -0,0 +1,12 @@
+#
+# This policy configuration will be used by samsung products
+#
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/common/vendor
