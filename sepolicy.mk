Stricted/android_device_exynos_sepolicy

tee: Add teegris_use() macro

sepolicy.mk
@@ -12,6 +12,9 @@ BOARD_SEPOLICY_DIRS += \
     device/exynos/sepolicy/common/vendor
 
 ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),teegris)
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/exynos/sepolicy/tee/teegris/public
+
 BOARD_SEPOLICY_DIRS += \
     device/exynos/sepolicy/tee/teegris/vendor
 endif

LineageOS/android_device_samsung_slsi_sepolicy

Change-Id: Ib72ddded545728eda7ad12d76e37687ea119e840

sepolicy.mk
@@ -12,6 +12,9 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung_slsi/sepolicy/common/vendor
 
 ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),teegris)
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/tee/teegris/public
+
 BOARD_SEPOLICY_DIRS += \
     device/samsung_slsi/sepolicy/tee/teegris/vendor
 endif