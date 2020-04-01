Stricted/android_device_exynos_sepolicy

tee: Add policy for teegris

sepolicy.mk
@@ -10,3 +10,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 BOARD_SEPOLICY_DIRS += \
     device/exynos/sepolicy/common/vendor
+
+ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),teegris)
+BOARD_SEPOLICY_DIRS += \
+    device/exynos/sepolicy/tee/teegris/vendor
+endif

LineageOS/android_device_samsung_slsi_sepolicy

Change-Id: I932448f021ba5da02d0469ff968529bc7981578a

sepolicy.mk
@@ -10,3 +10,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 BOARD_SEPOLICY_DIRS += \
     device/samsung_slsi/sepolicy/common/vendor
+
+ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),teegris)
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/tee/teegris/vendor
+endif