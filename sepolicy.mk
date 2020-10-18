252c987f8a793398adbd19fce61e6b0a00238f34
Jan Altensen <info@stricted.net>
2020-10-18 13:38:56 +0200

    mobicore: split into legacy and treble folders
    Change-Id: I44bdbc49944be89314f1f96d8a2c1c9fb58e1352

--- sepolicy.mk
+++ sepolicy.mk
@@ -18,6 +18,16 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_SEPOLICY_DIRS += \
     device/samsung_slsi/sepolicy/tee/teegris/vendor
 else ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),mobicore)
+
+# devices launched with oreo should be treble
+ifneq ($(call math_gt_or_eq,$(PRODUCT_SHIPPING_API_LEVEL),26),)
 BOARD_SEPOLICY_DIRS += \
-    device/samsung_slsi/sepolicy/tee/mobicore
+    device/samsung_slsi/sepolicy/tee/mobicore/treble
+else
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/tee/mobicore/legacy
+endif
+
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/tee/mobicore/common
 endif
